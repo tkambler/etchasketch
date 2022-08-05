8fd8b53deb31322041434044b7af235c5146c0e9
Tim Ambler <tkambler@gmail.com>
2022-08-05 08:55:39 -0500

    SessionService not needed at this time.

--- projects/session-service/SessionService.ts
+++
@@ -1,8 +0,0 @@
-import { Inject, Service } from 'typedi';
-import { KnexService } from '@talkspace/knex-service';
-
-@Service()
-export class SessionService {
-  @Inject()
-  private knex: typeof KnexService;
-}
