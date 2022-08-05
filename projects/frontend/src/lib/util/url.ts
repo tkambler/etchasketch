6532af9d3de969288fd9e049f574380bc6455dd6
Tim Ambler <tkambler@gmail.com>
2022-08-05 15:22:37 -0500

    Remove unused module

--- projects/frontend/src/lib/util/url.ts
+++
@@ -1,9 +0,0 @@
-import * as queryString from 'query-string';
-
-export function parseQueryParams(params?): Record<string, unknown> {
-  return queryString.parse(params || location.search);
-}
-
-export function stringifyQueryParams(parsed): string {
-  return queryString.stringify(parsed);
-}
